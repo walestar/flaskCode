02d9a4659fd46360e79370be41035aaf279083c0
walestar <77537713+walestar@users.noreply.github.com>
2023-10-27 03:55:37 -0400

    Update user.py

--- Controllers/user.py
+++ Controllers/user.py
@@ -1,14 +1,23 @@
 from App.models import User
 from App.database import db
 
-def create_user(userID, password):
-    newuser = User(userID=userID, password=password)
+def create_student(studentID, password):
+    newuser = User(studentID=studentID, password=password)
     db.session.add(newuser)
     db.session.commit()
     return newuser
 
-def get_user_by_userID(userID):
-    return User.query.filter_by(userID=userID).first()
+def create_staff(staffID, password):
+    newuser = User(staffID=staffID, password=password)
+    db.session.add(newuser)
+    db.session.commit()
+    return newuser
+
+def get_student_by_studentID(studentID):
+    return User.query.filter_by(studentID=studentID).first()
+
+def get_student_by_studentID(studentID):
+    return User.query.filter_by(staffID=staffID).first()
 
 def get_all_users():
     return User.query.all()
@@ -20,11 +29,17 @@ def get_all_users_json():
     users = [user.get_json() for user in users]
     return users
 
-def update_user(userID):
-    user = get_user(userID)
-    if user:
-        user.userID = userID
-        db.session.add(user)
+def update_student(studentID):
+    student = get_student(studentID)
+    if student:
+        student.studentID = studentID
+        db.session.add(student)
+
+def update_staff(stafftID):
+    staff = get_student(staffID)
+    if staff:
+        staff.staffID = staffID
+        db.session.add(staff)
         return db.session.commit()
     return None
     
